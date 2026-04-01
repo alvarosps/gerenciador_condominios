7efe6f6eb918fb0248cbc59cc1a7efbeb3ee1488
Alvaro S.P.S. <alvaro123sps@gmail.com>
2026-04-01 13:39:04 -0300

    fix(devices): include all fields in create_defaults for update_or_create

--- core/viewsets/device_views.py
+++ core/viewsets/device_views.py
@@ -57,7 +57,11 @@ class DeviceTokenViewSet(ViewSet):
                 "updated_by": authenticated_user,
             },
             create_defaults={
+                "user": authenticated_user,
+                "platform": platform,
+                "is_active": True,
                 "created_by": authenticated_user,
+                "updated_by": authenticated_user,
             },
         )
         return Response(
